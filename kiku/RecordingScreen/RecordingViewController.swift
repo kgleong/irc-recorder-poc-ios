b0b1037fa76d4ba685997f62e22bbb8d0091fd9e
Kevin Leong <kgleong@squareup.com>
2018-12-22 19:40:04 -0800

    Get POC working for testing.

--- kiku/RecordingScreen/RecordingViewController.swift
+++ kiku/RecordingScreen/RecordingViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import AVFoundation
 
 class RecordingViewController: UIViewController, AVAudioRecorderDelegate {
+    let consoleLabel = UILabel()
     let recordButton = UIButton()
     let microphonePermissionButton = UIButton()
     let playButton = UIButton()
@@ -41,9 +42,16 @@ class RecordingViewController: UIViewController, AVAudioRecorderDelegate {
         playButton.setTitleColor(UIColor.blue, for: .normal)
         playButton.addTarget(self, action: #selector(didTapPlay), for: .touchUpInside)
         
+        consoleLabel.translatesAutoresizingMaskIntoConstraints = false
+        consoleLabel.text = "Debug messages will appear here..."
+        consoleLabel.textColor = .black
+        consoleLabel.textAlignment = .center
+        consoleLabel.numberOfLines = 0
+        
         view.addSubview(recordButton)
         view.addSubview(microphonePermissionButton)
         view.addSubview(playButton)
+        view.addSubview(consoleLabel)
         
         NSLayoutConstraint.activate([
             recordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -51,43 +59,52 @@ class RecordingViewController: UIViewController, AVAudioRecorderDelegate {
             microphonePermissionButton.centerXAnchor.constraint(equalTo: recordButton.centerXAnchor),
             microphonePermissionButton.topAnchor.constraint(equalTo: recordButton.bottomAnchor, constant: 20),
             playButton.centerXAnchor.constraint(equalTo: recordButton.centerXAnchor),
-            playButton.topAnchor.constraint(equalTo: microphonePermissionButton.bottomAnchor, constant: 20)
+            playButton.topAnchor.constraint(equalTo: microphonePermissionButton.bottomAnchor, constant: 20),
+            consoleLabel.topAnchor.constraint(equalTo: playButton.bottomAnchor, constant: 20),
+            consoleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            consoleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
             ])
 
         audioSession = AVAudioSession.sharedInstance()
     }
     
     @objc private func didTapPlay() {
-        print("Play pressed")
+        consoleLabel.setTextAndPrint("Play button pressed")
         let fileURL = audioFileURL(name: "myaudiofile", fileExtension: "m4a")
+        
+        guard FileManager.default.fileExists(atPath: fileURL.path) else {
+            return
+        }
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
             audioPlayer?.prepareToPlay()
             audioPlayer?.play()
-        } catch let error as NSError {
-            fatalError(error.localizedDescription)
+        } catch {
+            consoleLabel.setTextAndPrint("Could not play file at \(fileURL.path)")
         }
     }
     
     @objc private func didTapRecord() {
         switch AVAudioSession.sharedInstance().recordPermission {
         case .granted:
-            print("Mic access granted.")
+            consoleLabel.setTextAndPrint("Mic access granted.")
             if isRecording {
-                print("Stopping recording")
+                recordButton.setTitle("Record", for: .normal)
+                consoleLabel.setTextAndPrint("Stopping recording")
                 audioRecorder?.stop()
                 isRecording = false
                 self.audioRecorder = nil
             } else {
                 isRecording = true
-                print("Starting recording")
+                recordButton.setTitle("Stop Recording", for: .normal)
+                consoleLabel.setTextAndPrint("Starting recording")
                 recordAudio()
             }
         case .denied:
-            print("Mic access denied.")
+            consoleLabel.setTextAndPrint("Mic Access Denied")
         case .undetermined:
-            print("Need to request mic access.")
+            consoleLabel.setTextAndPrint("Need to request mic access")
         }
     }
     
@@ -105,10 +122,9 @@ class RecordingViewController: UIViewController, AVAudioRecorderDelegate {
     
     private func recordAudio() {
         recordingSettings[AVEncoderAudioQualityKey] = AVAudioQuality.min.rawValue
-//        recordingSettings[AVEncoderBitRateKey] = 16
+        recordingSettings[AVEncoderBitRateKey] = 64000
         recordingSettings[AVNumberOfChannelsKey] = 1
         recordingSettings[AVSampleRateKey] = 44100
-//        recordingSettings[AVFormatIDKey] = Int(kAudioFormatAppleIMA4)
         recordingSettings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
 
         let audioRecordingURL = audioFileURL(name: "myaudiofile", fileExtension: "m4a")
@@ -122,12 +138,16 @@ class RecordingViewController: UIViewController, AVAudioRecorderDelegate {
         }
         
         do {
-//            try FileManager.default.removeItem(at: audioRecordingURL)
+            if FileManager.default.fileExists(atPath: audioRecordingURL.path) {
+                consoleLabel.setTextAndPrint("Removing file at \(audioRecordingURL)")
+                try FileManager.default.removeItem(at: audioRecordingURL)
+            }
             
             audioRecorder = try AVAudioRecorder(url: audioRecordingURL, settings: recordingSettings)
             audioRecorder?.delegate = self
             audioRecorder?.prepareToRecord()
             audioRecorder?.record()
+            consoleLabel.setTextAndPrint("Recording")
         } catch let error as NSError {
             fatalError(error.localizedDescription)
         }
@@ -143,7 +163,16 @@ class RecordingViewController: UIViewController, AVAudioRecorderDelegate {
     
     @objc private func didTapRequestMicAccess() {
         AVAudioSession.sharedInstance().requestRecordPermission { allowed in
-            print("Permission Granted: \(String(describing: allowed))")
+            self.consoleLabel.setTextAndPrint("Permission Granted: \(String(describing: allowed))")
+        }
+    }
+}
+
+extension UILabel {
+    func setTextAndPrint(_ text: String?) {
+        self.text = text
+        if let newText = text {
+            print(newText)
         }
     }
 }
